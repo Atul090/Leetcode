ae7da9d1ba0bbc1ed7b1f8ca6186890f9a68ce23
Atul Deep Singh <121950520+Atul090@users.noreply.github.com>
2023-08-16 22:52:34 +0530

    Time: 16 ms (32.96%), Space: 54.7 MB (24.71%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,27 +1,5 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        //using cyclic sort approach
-        List<Integer> result=new ArrayList<>();
-        int i=0;
-        while(i<nums.length){
-            int correct=nums[i]-1;
-            if(nums[i]!=nums[correct]){
-                if(correct<nums.length){
-                    int temp=nums[i];
-                    nums[i]=nums[correct];
-                    nums[correct]=temp;
-                }
-            }
-            else{
-                i++;
-            }
-        }
-        for(i=0;i<nums.length;i++){
-            if(nums[i]!=i+1)result.add(i+1);
-        }
-        return result;
-
-        /*using HashSet
         HashSet<Integer> set=new HashSet<>();
         ArrayList<Integer> result=new ArrayList<>();
         for(int num: nums){
@@ -31,6 +9,5 @@ class Solution {
             if(!set.contains(i+1))result.add(i+1);
         }
         return result;
-        */
     }
 }
